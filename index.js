2fc99af04e9f8ef4cb7361a02d832a005a343f14
Michael Boroski <mjboroski@gmail.com>
2018-07-08 02:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ function destructivelyRemoveFirstDriver(){
 }
 
 function appendDriver(name){
-  return drivers.push(name);
+  return [...drivers, name];
 }
 
 function prependDriver(name){
-
+  return [name, ...drivers];
 }
 
 function removeLastDriver(){
-
+  return drivers.slice(1);
 }
 
 function removeFirstDriver(){
-
+  return drivers.slice(0, drivers.length - 1);
 }
