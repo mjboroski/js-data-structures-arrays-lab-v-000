fbc35601ca8d0ef671f07e2b80cfeb0eaf7f5be5
Michael Boroski <mjboroski@gmail.com>
2018-07-08 02:43:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const drivers = ["Milo", "Otis", "Garfield"];
 
+function destructivelyAppendDriver(name){
+  
+}
+
+function destructivelyPrependDriver(name){
+  
+}
+
+function destructivelyRemoveLastDriver(){
+  
+}
+
+function destructivelyAppendDriver(name){
+  
+}
+
+function destructivelyAppendDriver(name){
+  
+}
+
+function destructivelyAppendDriver(name){
+  
+}
+
+function destructivelyAppendDriver(name){
+  
+}
+
+function destructivelyAppendDriver(name){
+  
+}
